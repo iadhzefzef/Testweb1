5418f5a064a69e710345c2ef0234556b2769e983
nYYx00 <110718286+iadhzefzef@users.noreply.github.com>
2022-10-15 12:01:50 +0100

    Update main.js

--- Sport/main.js
+++ Sport/main.js
@@ -1,1 +1,16 @@
-document.querySelectorAll('.btn-modal')
+var closebtn = document.getElementById("close-btn");
+var modalContainer = document.querySelector(".modal-bg");
+var images = document.getElementsByClassName("imag");
+var imageContaoner = document.getElementById("image-container");
+closebtn.addEventListener("click", function () {
+  modalContainer.style.display = "none";
+});
+
+for (let i = 0; i < images.length; i++) {
+  let im = images[i];
+
+  im.addEventListener("click", function () {
+    imageContaoner.src = im.src;
+    modalContainer.style.display = "flex";
+  });
+}
